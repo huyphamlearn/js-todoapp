db5333a1d1f1641b3b28207d0c9a0453db0f2160
stevephamhi <hung.pham@cubicstack.net>
2024-03-27 07:23:59 +0700

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * indexes -> array
+ * 
+ * numbers = [10, 2, 3, 4, 5, 6, 7]
+ *            0   1  2  3  4  5  6
+ * -> lenght = 7
+ * 
+ * 
+ * lay ra phan tu co index = 5
+ * 
+ * numbers[5] = 6
+ */
